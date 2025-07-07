e02ca2a981667bada644808e517c40cfdd96a584
aydie <aydieprodstudio@gmail.com>
2025-07-07 22:55:48 +0530

    Updated template and files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "US-Visa-Approval-Prediction",
+    version = "1.0.0",
+    author = "Aditya Dinesh (Aydie)",
+    author_email = "business@aydie.in",
+    packages = find_packages()
+)
