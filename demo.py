c84a3c7904a793e8272fb5847db8cbdec2bbda48
aydie <aydieprodstudio@gmail.com>
2025-07-08 19:21:56 +0530

    Updated the exception

--- demo.py
+++ demo.py
@@ -1,3 +1,11 @@
 from usa_visa.logger import logging
+from usa_visa.exception import AydieMLException
+import sys
+
 
 logging.info("Welcome to my custom log")
+
+try:
+    a = 2/0
+except Exception as e:
+    raise AydieMLException(e, sys)
