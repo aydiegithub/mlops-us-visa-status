01124717c44edbe48af396c6eaca921cf92de5a4
aydie <aydieprodstudio@gmail.com>
2025-07-08 18:33:12 +0530

    Created notebook and checked mongodb connections

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "US-Visa-Approval-Prediction",
+    name = "us-visa",
     version = "1.0.0",
     author = "Aditya Dinesh (Aydie)",
     author_email = "business@aydie.in",
