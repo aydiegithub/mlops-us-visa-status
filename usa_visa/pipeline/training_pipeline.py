505eb23105cc6c5700d3380a7e0d9b38a2b79cd4
aydie <aydieprodstudio@gmail.com>
2025-07-11 21:50:34 +0530

    Updated Data Validation inside Training Pipeline

--- usa_visa/pipeline/training_pipeline.py
+++ usa_visa/pipeline/training_pipeline.py
@@ -3,12 +3,13 @@ import sys
 from usa_visa.exception import AydieException
 from usa_visa.logger import logging
 from usa_visa.components.data_ingestion import DataIngestion
-from usa_visa.entity.config_entity import DataIngestionConfig
-from usa_visa.entity.artifact_entity import DataIngestionArtifact
+from usa_visa.entity.config_entity import DataIngestionConfig, DataValidationConfig
+from usa_visa.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
 
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
+        self.data_validation_config = DataValidationConfig()
         
     def start_data_ingestion(self) -> DataIngestionArtifact:
         """
@@ -22,7 +23,7 @@ class TrainPipeline:
             logging.info("Entered the [start_data_ingestion] method of TrainPipelin class")
             logging.info("Getting data from mongo db")
             data_ingestion = DataIngestion(data_ingestion_config = self.data_ingestion_config)
-            data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+            data_validation_artifact = self.start_data_validation(data_ingestion_artifact = data_ingestion_artifact)
             logging.info("Got the train_set and test_set from mongodb")
             logging.info(
                 "Exited the start_data_ingestion method of TrainPipeline class"
@@ -33,6 +34,25 @@ class TrainPipeline:
         except Exception as e:
             raise AydieException(e, sys) from e
         
+    def start_data_validation(self, data_ingestion_artifact: DataIngestionArtifact) -> DataValidationArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting data validation component
+        """
+        
+        logging.info("Entered the start_data_validation method of TrainPipeline class")
+        try:
+            data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,
+                                                data_validation_config=self.data_validation_config
+                                                )
+            data_validation_artifact = data_validation.initiate_data_validation()
+            logging.info("Performed the data validation operation")
+            logging.info(
+                "Exited the start_data_validation method of TrainPipeline class"
+            )
+            return data_validation_artifact
+
+        except Exception as e:
+            raise AydieException(e, sys) from e
         
         
     def run_pipeline(self, ) -> None:
@@ -42,8 +62,8 @@ class TrainPipeline:
         try:
             logging.info("Entered the [run_pipeline] method of TrainPipelin class")
             data_ingestion_artifact = self.start_data_ingestion()
+            data_validation_artifact = self.start_data_validation(data_ingestion_artifact = data_ingestion_artifact)
             logging.info("Exited the [run_pipeline] method of TrainPipelin class")
-            return data_ingestion_artifact
             
         except Exception as e:
             raise AydieException(e, sys) from e
