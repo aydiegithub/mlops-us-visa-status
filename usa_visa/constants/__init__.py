c60e56d1793e0e778b1e4a298259e1a15d0cb9dc
aydie <aydieprodstudio@gmail.com>
2025-07-11 19:14:13 +0530

    Updated following: constants with datavalidation, schema yaml, config entity

--- usa_visa/constants/__init__.py
+++ usa_visa/constants/__init__.py
@@ -19,6 +19,12 @@ FILE_NAME: str = "usvisa.parquet"
 
 MODEL_FILE_NAME = "model.pkl"
 
+TARGET_COLUMN = "case_status"
+CURRENT_YEAR = date.today().year
+PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
+SCHEMA_FILE_PATH_URL = os.path.join("config", "schema.yaml")
+
+#############################################################################################
 
 # Data Ingestion Constants
 DATA_INGESTION_COLLECTION_NAME: str = COLLECTION_NAME
@@ -26,3 +32,10 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
+
+#############################################################################################
+
+# Data Validation Constants
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
