6f6bc9fe259702785356a327a5ac7ed9f44cbb54
aydie <aydieprodstudio@gmail.com>
2025-07-12 18:01:10 +0530

    updated main_utils with numpy write object

--- usa_visa/utils/main_utils.py
+++ usa_visa/utils/main_utils.py
@@ -13,8 +13,8 @@ from usa_visa.logger import logging
 
 # This is custom function to read the yaml files
 def read_yaml_file(file_path: str) -> dict:
-    logging.info("Entered the read_yaml_file method of utils")
     try:
+        logging.info("Entered the read_yaml_file method of utils")
         with open(file_path, "rb") as yaml_file:
             return yaml.safe_load(yaml_file)
         
@@ -28,8 +28,8 @@ def read_yaml_file(file_path: str) -> dict:
 
 # This is custom function to write the yaml files
 def write_yaml_file(file_path: str, content: object, replace: bool = False) -> None:
-    logging.info("Entered the write_yaml_file method of utils")
     try:
+        logging.info("Entered the write_yaml_file method of utils")
         if replace: 
             if os.path.exists(file_path):
                 os.remove(file_path)
@@ -47,8 +47,8 @@ def write_yaml_file(file_path: str, content: object, replace: bool = False) -> N
 
 # This is used to load the custom objects
 def load_object(file_path: str) -> object:
-    logging.info("Entered the load_object method of utils")
     try:
+        logging.info("Entered the load_object method of utils")
         with open(file_path, "rb") as file_obj:
             obj = dill.load(file_obj)
             
@@ -63,8 +63,8 @@ def load_object(file_path: str) -> object:
  
 # This is used to save the custom objects
 def save_object(file_path: str, obj: object) -> None:
-    logging.info("Entered the save_object method of utils")
     try:
+        logging.info("Entered the save_object method of utils")
         os.makedirs(os.path.dirname(file_path), exist_ok=True)
         with open(file_path, "wb") as file_obj:
             dill.dump(obj, file_obj)
@@ -79,9 +79,8 @@ def save_object(file_path: str, obj: object) -> None:
 
 # This is used to drop specified columns from a DataFrame
 def drop_columns(df: pd.DataFrame, cols: list) -> pd.DataFrame:
-    logging.info("Entered the drop_columns method of utils")
-    
-    try:
+    try:    
+        logging.info("Entered the drop_columns method of utils")
         df_dropped = df.drop(columns=cols, axis=1)
         logging.info(f"Dropped columns: {cols}")
         return df_dropped
@@ -93,10 +92,9 @@ def drop_columns(df: pd.DataFrame, cols: list) -> pd.DataFrame:
 
 
 # This is used to load numpy array data from a file
-def load_numpy_array_data(file_path: str) -> np.array:
-    logging.info("Entered the load_numpy_array_data method of utils")
-    
+def load_numpy_array_data(file_path: str) -> np.array:    
     try:
+        logging.info("Entered the load_numpy_array_data method of utils")
         with open(file_path, 'rb') as file_obj:
             array = np.load(file_obj)
         logging.info("Exited the load_numpy_array_data method of utils")
@@ -104,3 +102,17 @@ def load_numpy_array_data(file_path: str) -> np.array:
     
     except Exception as e:
         raise AydieException(e, sys) from e
+    
+
+
+
+# This is used to save numpy array data to a npy
+def save_numpy_array_data(file_path: str, array: np.array):
+    try:
+        logging.info("Entered the load_numpy_array_data method of utils")
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, 'wb') as file_obj:
+            np.save(file_obj, array)
+    except Exception as e:
+        raise USvisaException(e, sys) from e
