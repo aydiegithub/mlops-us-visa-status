ea32d1679434f4e962deae3ec0448c206bc60f60
aydie <aydieprodstudio@gmail.com>
2025-07-12 19:04:43 +0530

    Updated training pipeline with data transformation

--- usa_visa/pipeline/training_pipeline.py
+++ usa_visa/pipeline/training_pipeline.py
@@ -2,15 +2,19 @@ import sys
 
 from usa_visa.exception import AydieException
 from usa_visa.logger import logging
+
 from usa_visa.components.data_ingestion import DataIngestion
 from usa_visa.components.data_validation import DataValidation
-from usa_visa.entity.config_entity import DataIngestionConfig, DataValidationConfig
-from usa_visa.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
+from usa_visa.components.data_transformation import DataTransformation
+
+from usa_visa.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
+from usa_visa.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact, DataTransformationArtifact
 
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
         self.data_validation_config = DataValidationConfig()
+        self.data_transformation_config = DataTransformationConfig()
         
     def start_data_ingestion(self) -> DataIngestionArtifact:
         """
@@ -55,6 +59,23 @@ class TrainPipeline:
         except Exception as e:
             raise AydieException(e, sys) from e
         
+    
+    def start_data_transformation(self, data_ingestion_artifact: DataIngestionArtifact, data_validation_artifact: DataValidationArtifact) -> DataTransformationArtifact:
+        """
+        This method of TrainingPipeline class is responsible for starting data transformation component
+        """
+        try:
+            data_transformation = DataTransformation(
+                                        data_ingestion_artifact = data_ingestion_artifact,
+                                        data_transformation_config = self.data_transformation_config,
+                                        data_validation_artifact = data_validation_artifact
+                                )
+            data_transformation_artifact = data_transformation.initiate_data_transformation()
+            return data_ingestion_artifact
+        except Exception as e:
+            raise AydieException(e, sys)
+        
+        
         
     def run_pipeline(self, ) -> None:
         """
@@ -64,6 +85,9 @@ class TrainPipeline:
             logging.info("Entered the [run_pipeline] method of TrainPipelin class")
             data_ingestion_artifact = self.start_data_ingestion()
             data_validation_artifact = self.start_data_validation(data_ingestion_artifact = data_ingestion_artifact)
+            data_transformation_artifact = self.start_data_transformation(
+                data_ingestion_artifact = data_ingestion_artifact, data_validation_artifact = data_validation_artifact
+            )
             logging.info("Exited the [run_pipeline] method of TrainPipelin class")
             
         except Exception as e:
